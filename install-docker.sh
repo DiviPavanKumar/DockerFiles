e202809f180b9eabc70ed17716a03417be846bae
divipavankumar92 <147476470+divipavankumar92@users.noreply.github.com>
2025-04-19 08:22:51 +0530

    Docker_Installation

--- install-docker.sh
+++ install-docker.sh
@@ -1,7 +1,28 @@
-#!/bin/bash
+date_var=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(basename "$0")
+LOGFILE="/tmp/${SCRIPT_NAME}-${date_var}.log"
 
-LOGFILE="/var/log/docker-install.log"
-exec > >(tee -a "$LOGFILE") 2>&1
+# Define color codes for output
+R="\e[31m"  # Red (Failure)
+G="\e[32m"  # Green (Success)
+Y="\e[33m"  # Yellow (Info)
+N="\e[0m"   # Reset color
+
+# Check if the script is run as root
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e "${R}Error: Please run this script with sudo access.${N}"
+    exit 1
+fi
+
+# Validation function
+VALIDATE() {
+    if [ $? -ne 0 ]; then
+        echo -e "$2 ..... ${R}Failed${N}"
+        exit 1
+    else
+        echo -e "$2 ...... ${G}Successful${N}"
+    fi
+}
 
 echo "Starting Docker installation at $(date)" &>> $LOGFILE
 
